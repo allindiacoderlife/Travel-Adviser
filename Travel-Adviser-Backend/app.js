1790f0453d2545a813eb2d73a491d5283c2bea7b
allindiacoderlife <chiragsaxena728@gmail.com>
2024-11-09 06:41:23 +0530

    update

--- Travel-Adviser-Backend/app.js
+++ Travel-Adviser-Backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const ollama = require('./router/ollama');
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.use('/', ollama);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
